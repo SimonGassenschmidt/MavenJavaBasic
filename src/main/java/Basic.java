cbee76915020459a90a70ed12268ab30366cedaa
simongassenschmidt <simon.gassenschmidt@gmx.net>
2021-04-05 15:00:03 +0200

    fibu and more

--- src/main/java/Basic.java
+++ src/main/java/Basic.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-
+import java.util.Arrays;
 /**
  * @author Simon
  *
@@ -43,10 +43,22 @@ public class Basic {
 		//System.out.println(e);
                 
                  int[] values_2 =
-        {-180, -170, -190, -120, 0, 40, -30, 50, 60, -40, -30, -30, 0, -10, -20, 20, 40, 50, 20,
-            -40, -30, -30, 40, 100, 240, 260, 180, 200, 280, 290, 260, 240, 250, 270, 240, 200, 120,
-            30, -20, -80, 20, 30, 30, 0, -20, -40, -60, -80, -120, -160, -180, -100};
-                 //System.out.println(Basic.getMaximumOfSubarray(values_2));
+                    {-180, -170, -190, -120, 0, 40, -30, 50, 60, -40, -30, -30, 0, -10, -20, 20, 40, 50, 20,
+                    -40, -30, -30, 40, 100, 240, 260, 180, 200, 280, 290, 260, 240, 250, 270, 240, 200, 120,
+                    30, -20, -80, 20, 30, 30, 0, -20, -40, -60, -80, -120, -160, -180, -100};
+                 //System.out.println(Arrays.toString(values_2));
+                //System.out.println(Basic.getMaximumOfSubarray(values_2));
+                
+                //System.out.println(Basic.getSumofMultiplesofValueUnderMax(3, 10));
+                //System.out.println(Basic.getSumofMultplesof2ValuesUnderMax(3, 5, 10));
+                //System.out.println(Basic.getSumofMultplesof2ValuesUnderMax(3, 5, 1000));
+                
+                //System.out.println(Basic.isEven(4));
+                //System.out.println(Basic.isEven(3));
+                //System.out.println(Basic.isOdd(4));
+                //System.out.println(Basic.isOdd(3));
+                
+                //System.out.println(Basic.getFibunaci(10));
 	}
 	public static int getAddition (int a, int b) {
 		return a+b;
@@ -103,5 +115,61 @@ public class Basic {
                 }
             }
             return maximum;
+        }
+        
+        public static int getMinimumValueofArray(int[] values){
+            int minimum = 0;
+            for(int value : values){
+                if(value < minimum){
+                    minimum = value;
+                }
+            }
+            return minimum;
+        }
+        public static int getMaximumValueofArray(int[] values){
+            int maximum = 0;
+            for(int value : values) {
+                if(value > maximum){
+                    maximum = value;
+                }
+            }
+            return maximum;
+        }
+        public static int getSumofMultiplesofValueUnderMax (int value, int max) {
+            int sum = 0;
+            for (int i = 1; i < max; i++){
+                if((i % value) == 0){
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+        public static int getSumofMultplesof2ValuesUnderMax(int value1, int value2, int max) {
+            int sum = 0;
+            for (int i = 1; i < max; i++){
+                if(((i % value1) == 0) || ((i % value2) == 0)){
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+        public static boolean isEven(int value) {
+            return (value % 2) == 0;
+        }
+        public static boolean isOdd(int value) {
+            return !Basic.isEven(value);
+        }
+        public static int getFibunaci (int nth) {
+            if(nth == 0) {
+                return 0;
+            }
+            else if(nth == 1) {
+                return 1;
+            }
+            else {
+                int sum = 0;
+                sum = Basic.getFibunaci(nth-1)+Basic.getFibunaci(nth-2);
+                return sum;
+            }
         }
 }
